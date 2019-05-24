48ac7286898e0e0c0b6d3d51694ea9b825595bef
layla dantas <laylatdantas@gmail.com>
2019-05-24 03:39:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,13 @@
 
 def key_for_min_value(name_hash)
   lowest_value=9999
+  lowest_shit = "fuck"
   name_hash.each do  |k, v|
-   
+    if v<lowest_value 
+      lowest_value = v
+      lowest_shit = k 
+    end
+  end
+  puts lowest_shit
+  lowest_shit
 end
