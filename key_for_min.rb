1cbd2e175d9e9036869e35340d0e17d6b1abc3e8
layla dantas <laylatdantas@gmail.com>
2019-05-24 03:41:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return if name_hash.nil?
+  
   lowest_value=9999
-  lowest_shit = "fuck"
+  lowest_shit = nil
   name_hash.each do  |k, v|
     if v<lowest_value 
       lowest_value = v
