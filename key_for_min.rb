13401d79c73d7e0f3be7382e4372f11a40dc7b7a
layla dantas <laylatdantas@gmail.com>
2019-05-24 03:43:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,13 @@
 
 def key_for_min_value(name_hash)
   
-  lowest_value=9999
-  lowest_shit = nil
+  minValue=9999
+  minKey= nil
   name_hash.each do  |k, v|
-    if v<lowest_value 
-      lowest_value = v
-      lowest_shit = k 
+    if v< minValue
+      minValue = v
+      minKey = k 
     end
   end
-  lowest_shit
+  minKey
 end
